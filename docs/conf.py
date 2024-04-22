585a0546adc171b30018a8fbe064f3905b39bc8b
janezd <janez.demsar@fri.uni-lj.si>
2024-04-22 09:54:22 +0200

    Add .readthedocs.yaml

--- docs/conf.py
+++ docs/conf.py
@@ -173,15 +173,3 @@ texinfo_documents = [
 
 
 autodoc_member_order = 'bysource'
-
-import sys
-from unittest.mock import MagicMock
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-            return MagicMock()
-
-MOCK_MODULES = ['numpy', 'scipy']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
-import baycomp
